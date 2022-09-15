986c97d0c8934df06afcd438c8ea4d2e582dcfb0
Bhupinder Singh <b.bains1986@gmail.com>
2022-09-16 00:10:49 +0100

    Title added to iframe element in APP to allowing CD/CI on netlify

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
     <p id="gift">Site Under Construction</p>
-    <iframe id= "present" src="https://giphy.com/embed/l3vRebb6HyeIgvmQ8" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+    <iframe id= "present" title="This is a unique title" src="https://giphy.com/embed/l3vRebb6HyeIgvmQ8" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
      <p id="gift">Site Under Construction</p>
     </div>
   );
