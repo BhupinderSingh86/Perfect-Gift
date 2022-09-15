b7f68ba269bfb3cc5ca9c5693e437b98159d6339
Bhupinder Singh <b.bains1986@gmail.com>
2022-09-15 23:59:53 +0100

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <p id="gift">Site Under Construction</p>
+    <iframe id= "present" src="https://giphy.com/embed/l3vRebb6HyeIgvmQ8" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+     <p id="gift">Site Under Construction</p>
     </div>
   );
 }
