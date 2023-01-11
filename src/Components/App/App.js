c816ec2b773bb92d22c743ec095fb5c745842b9d
Bhupinder Singh <b.bains1986@gmail.com>
2023-01-11 11:08:21 +0000

    update

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import ImageAvatar from "../ImageAvatar/ImageAvatar";
 import ProfileBackground from "../ProfileBackground/ProfileBackground";
+import SignUpForm from "../SignUpForm/SignUpForm";
+
 
 function App() {
   return (
     <div>
     <ProfileBackground/>
     <ImageAvatar/>
-
+    <SignUpForm/>
     </div>
   );
 }
