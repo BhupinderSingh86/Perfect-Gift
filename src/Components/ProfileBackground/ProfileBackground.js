a9548b90a4031d694fc3d0ca004e248a6a9db093
Bhupinder Singh <b.bains1986@gmail.com>
2023-01-11 12:32:12 +0000

    update after merge

--- src/Components/ProfileBackground/ProfileBackground.js
+++ src/Components/ProfileBackground/ProfileBackground.js
@@ -1,23 +1,22 @@
 import React from 'react'
-
-import IconButton from '@mui/material/IconButton';
-import {useState} from "react"
-import { Container, Typography } from '@mui/material';
-
-//when login is set up include a iternary if statement for the styled badge to show when logged on otherwise off
-
-function ProfileBackground() {
-
-  const [profile, setProfile] = useState(
-    0
-);
-
-function handleChange(event) {
-    setProfile(URL.createObjectURL(event.target.files[0]));
-
 import {useState} from "react"
 import "./ProfileBackground.css"
 import Button from '@mui/material/Button'
+import IconButton from '@mui/material/IconButton';
+import { Container, Typography } from '@mui/material';
+
+// //when login is set up include a iternary if statement for the styled badge to show when logged on otherwise off
+
+// function ProfileBackground() {
+
+//   const [profile, setProfile] = useState(
+//     0
+// );
+
+// function handleChange(event) {
+//     setProfile(URL.createObjectURL(event.target.files[0]));
+
+
 
 
 function ProfileBackground() {
@@ -33,11 +32,10 @@ function handleChange(event) {
 
 
     return (
-
+        <>
         <Container>
         <Typography variant="h4" component="h1" color="secondary" align="center">Click below to add your personalised background</Typography>
         </Container>
-
         <div id="border">
         <img alt= "" src={selectedImage}/>
         <Button id="button" variant="contained" component="label" size="small" >
@@ -45,7 +43,7 @@ function handleChange(event) {
         <input hidden accept="image/*" type="file" onChange={handleChange}/>
         </Button>
         </div>
-
+        </>
     )
 }
 
